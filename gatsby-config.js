ac05689125c3ba424f99c9a9fe6ceae16b546848
Shane Burke <shane.burke@simlocal.com>
2021-05-26 21:37:57 +0100

    fix: removed annoymise ip

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,11 +76,11 @@ module.exports = {
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
-        gtagConfig: {
-          optimize_id: "OPT_CONTAINER_ID",
-          anonymize_ip: true,
-          cookie_expires: 100,
-        },
+        // gtagConfig: {
+        //   optimize_id: "OPT_CONTAINER_ID",
+        //   anonymize_ip: true,
+        //   cookie_expires: 100,
+        // },
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body

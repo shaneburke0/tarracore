09b081ff7ec739daaa98f0af6736992b7792e5c7
Shane Burke <shane.burke@simlocal.com>
2021-05-28 23:41:21 +0100

    fix: updated env vars

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,38 +1,38 @@
 function slugify(string) {
   const a =
-    "àáäâãåăæąçćčđďèéěėëêęğǵḧìíïîįłḿǹńňñòóöôœøṕŕřßşśšșťțùúüûǘůűūųẃẍÿýźžż·/_,:;"
+    "àáäâãåăæąçćčđďèéěėëêęğǵḧìíïîįłḿǹńňñòóöôœøṕŕřßşśšșťțùúüûǘůűūųẃẍÿýźžż·/_,:;";
   const b =
-    "aaaaaaaaacccddeeeeeeegghiiiiilmnnnnooooooprrsssssttuuuuuuuuuwxyyzzz------"
-  const p = new RegExp(a.split("").join("|"), "g")
+    "aaaaaaaaacccddeeeeeeegghiiiiilmnnnnooooooprrsssssttuuuuuuuuuwxyyzzz------";
+  const p = new RegExp(a.split("").join("|"), "g");
 
   const slug = string
     .toString()
     .toLowerCase()
     .replace(/\s+/g, "-") // Replace spaces with -
-    .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
+    .replace(p, (c) => b.charAt(a.indexOf(c))) // Replace special characters
     .replace(/&/g, "-and-") // Replace & with 'and'
     .replace(/[^\w-]+/g, "") // Remove all non-word characters
     .replace(/--+/g, "-") // Replace multiple - with single -
     .replace(/^-+/, "") // Trim - from start of text
-    .replace(/-+$/, "") // Trim - from end of text
+    .replace(/-+$/, ""); // Trim - from end of text
 
-  return slug.charAt(0) === "/" ? slug : `/${slug}`
+  return slug.charAt(0) === "/" ? slug : `/${slug}`;
 }
 
 function titleIfy(slug) {
-  var words = slug.split("-")
+  var words = slug.split("-");
   for (var i = 0; i < words.length; i++) {
-    var word = words[i]
-    words[i] = word.charAt(0).toUpperCase() + word.slice(1)
+    var word = words[i];
+    words[i] = word.charAt(0).toUpperCase() + word.slice(1);
   }
-  return words.join(" ")
+  return words.join(" ");
 }
 
 function getTrimmedString(string, length = 8) {
   if (string.length <= length) {
-    return string
+    return string;
   } else {
-    return string.substring(0, length) + "..."
+    return string.substring(0, length) + "...";
   }
 }
 
@@ -40,7 +40,11 @@ function numberFormat(number) {
   return new Intl.NumberFormat("en-IE", {
     style: "currency",
     currency: "EUR",
-  }).format(number)
+  }).format(number);
 }
 
-export { slugify, titleIfy, getTrimmedString, numberFormat }
+function isBoolTrue(str) {
+  return str && str.toLowerCase() === "true";
+}
+
+export { slugify, titleIfy, getTrimmedString, numberFormat, isBoolTrue };

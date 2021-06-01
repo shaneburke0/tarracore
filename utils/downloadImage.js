e43a340c061384b73631d1f90ecf0b0019c9a175
Shane Burke <shane.burke@simlocal.com>
2021-06-01 11:09:16 +0100

    fix: updated images

--- utils/downloadImage.js
+++ utils/downloadImage.js
@@ -1,39 +1,40 @@
-import fs from "fs"
-import axios from "axios"
-import path from "path"
+import fs from "fs";
+import axios from "axios";
+import path from "path";
 
 function getImageKey(url) {
-  const split = url.split("/")
-  const key = split[split.length - 1]
-  const keyItems = key.split("?")
-  const imageKey = keyItems[0]
-  return imageKey
+  const split = url.split("/");
+  const key = split[split.length - 1];
+  const imagePrefix = split[split.length - 2];
+  const keyItems = key.split("?");
+  const imageKey = keyItems[0];
+  return `${imagePrefix}/${imageKey}`;
 }
 
 function getPathName(url, pathName = "downloads") {
-  let reqPath = path.join(__dirname, "..")
-  let key = getImageKey(url)
-  key = key.replace(/%/g, "")
-  const rawPath = `${reqPath}/public/${pathName}/${key}`
-  return rawPath
+  let reqPath = path.join(__dirname, "..");
+  let key = getImageKey(url);
+  key = key.replace(/%/g, "");
+  const rawPath = `${reqPath}/public/${pathName}/${key}`;
+  return rawPath;
 }
 
 async function downloadImage(url) {
-  console.log("*** DOWNLOAD IMAGE ***")
-  console.log("url", url)
+  console.log("*** DOWNLOAD IMAGE ***");
+  console.log("url", url);
   return new Promise(async (resolve, reject) => {
-    const path = getPathName(url)
-    console.log("path", path)
-    const writer = fs.createWriteStream(path)
+    const path = getPathName(url);
+    console.log("path", path);
+    const writer = fs.createWriteStream(path);
     const response = await axios({
       url,
       method: "GET",
       responseType: "stream",
-    })
-    response.data.pipe(writer)
-    writer.on("finish", resolve)
-    writer.on("error", reject)
-  })
+    });
+    response.data.pipe(writer);
+    writer.on("finish", resolve);
+    writer.on("error", reject);
+  });
 }
 
-export default downloadImage
+export default downloadImage;
